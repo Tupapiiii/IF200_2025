c50bbad10931054417dd9f80117476c00cd4bd7e
Alvin Gómez <alvto2005@gmail.com>
2025-08-14 11:26:04 -0600

    Mi primer commit en java

---
+++ IF200_IntroduccionProgra/src/if200_introduccionprogra/IF200_IntroduccionProgra.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if200_introduccionprogra;
+
+/**
+ *
+ * @author alvin
+ */
+public class IF200_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Dta types
+        
+        //Python
+        //print('Hola Mundo');
+        
+        
+        /**
+         * Tipos de datos númericos:
+         * int, long, short, float,
+         * numeric, money, decimal
+         * 
+         * 
+         * Manejo de fecha:
+         * Date
+         * 
+         * Cadenas de caracteres:
+         * String, char
+         * 
+         * Objetos:
+         * Object
+         * 
+         * Data structures
+         * Arrays or Vectors: int[] x;
+         *                    int[][] y;
+         * 
+         * True/False
+         * boolean
+         * 
+         */
+        
+    }
+    
+}
